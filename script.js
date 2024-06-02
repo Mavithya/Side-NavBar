c1806f8bd7243765d6662573b505ab97b4d6a2cf
MIHIRUD K.M.M <148643820+Mavithya@users.noreply.github.com>
2024-06-02 19:19:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,55 @@ modeSwtich.addEventListener("click", () => {
     modeText.innerHTML = "Dark Mode";
   }
 });
+
+/* ======Carousal Wallpapers====== */
+  
+    const carousel = document.querySelector('.carousel');
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const bgBlur = document.querySelector('.bg-blur');
+    const nextButton = document.getElementById('next');
+    const prevButton = document.getElementById('prev');
+    let currentIndex = 0;
+
+    function updateCarousel() {
+      const offset = -currentIndex * 100;
+      carousel.style.transform = `translateX(${offset}%)`;
+      const currentImgSrc = carouselItems[currentIndex].querySelector('img').src;
+      bgBlur.style.backgroundImage = `url(${currentImgSrc})`;
+    
+    }
+    function autoSlide() {
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      updateCarousel();
+    }
+
+    nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex + 1) % carouselItems.length;
+      updateCarousel();
+    });
+
+    const button = document.querySelector('.button');
+
+    prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+      // if (currentIndex < 0) {
+      //   currentIndex = carouselItems.length + 1;
+      // }
+      updateCarousel();
+    });
+
+
+    setInterval(autoSlide, 100000); // Change image every 3 seconds
+
+    updateCarousel(); // Initial update
+
+/*Button*/
+const Button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+button.classList.add("animate");
+setTimeout(() =>{
+  button.classList.remove("animate");
+},600);
+});
